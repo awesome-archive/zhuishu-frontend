91d0633a8dce3932deffe8eeab2abc9ac659f703
duxinlong <duxinlong@baidu.com>
2017-04-18 19:54:33 +0800

    add js-beautify and eslint config file

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <p>This is my first react App</p>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
